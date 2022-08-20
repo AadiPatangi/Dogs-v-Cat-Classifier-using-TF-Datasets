057edcd11b904c58c6880d85b3123d9f160aa9cb
Aadi Patangi <vpatangi75@gmail.com>
2022-08-20 15:58:17 -0700

    basic web app

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import cv2 as cv
 from PIL import Image
+import streamlit as st
+import os
+Work_Dir = "/Users/aadipatangi/Desktop/Python-Projects/Dogs v Cats Tensorflow/Dogs-v-Cat-Classifier-using-TF-Datasets/uploaded_images"
+IMG_SIZE = 100
 model = keras.models.load_model('/Users/aadipatangi/Desktop/Python-Projects/Dogs v Cats Tensorflow/Dogs-v-Cat-Classifier-using-TF-Datasets/content/saved_modelfinalz/my_model')
-print(model.summary(""))
+
+
+st.markdown("# [Dog or Cat](https://github.com/AadiPatangi/Dogs-v-Cat-Classifier-using-TF-Datasets)")
+st.markdown("### **By: Aadi Patangi**")
+image_file = st.file_uploader("Upload an image: ",type=["png","jpg","jpeg"])
